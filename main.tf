7e6d8fdafe7e78980f8e09242a67631c96049f85
anikpujan <anikpujan@gmail.com>
2021-06-16 23:26:25 +0000

    Terraform and helm

---
+++ main.tf
@@ -0,0 +1,29 @@
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+# provider "kubernetes" {
+#   config_path = "~/.kube/config"
+# }
+# ###Create  namespace
+# resource "kubernetes_namespace" "monitoring" {
+#   metadata {
+#     name = "monitoring"
+#   }
+# }
+###Deploy Prometheus
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  chart      = "prometheus"
+  # namespace  = "monitoring"
+}
+### Deploy Grafana
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  chart      = "grafana"
+  # namespace  = "monitoring"
+}
+
